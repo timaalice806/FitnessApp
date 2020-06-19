10adb5c5432bbf20dad2ed0589ef9e6d1b607cd2
Nicolas Timm <nntimm1@gmail.com>
2020-06-19 15:29:29 -0500

    Co-authored-by: vastyle2010@gmail.com <vastyle2010@gmail.com>

--- BackEnd-API/index.js
+++ BackEnd-API/index.js
@@ -1,12 +1,32 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+require("dotenv").config();
 
 app.get("/", (req, res) => {
-    res.send("Hello pretty World")
+  res.send("Hello pretty World");
 });
 
-const Port = process.env.PORT || 5000
+mongoose.connect(process.env.MONGO_URI, () => {
+  console.log("database is connected");
+});
+
+const connectDB = async () => {
+  try {
+    const connect = mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Db has been connected");
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+const Port = process.env.PORT || 5000;
 
 app.listen(Port, () => {
-    console.log(`application running on port ${Port}`);
+  console.log(`application running on port ${Port}`);
 });
